7a0b849de05cfda7303b1e44d73befb7736f0bd8
Syed Aman <sna274@nyu.edu>
2016-10-03 10:08:28 -0400

    start two sum problem

---
+++ src/twoSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var numsLength = nums.length;
+    for (var addedIndex = 0; nums[addedIndex++]; addedIndex++){
+        for (var adderIndex = 0; adderIndex != addedIndex && nums[adderIndex++]; adder++) {
+
+        }
+        num[multiplicandIndex]
+    }
+};
