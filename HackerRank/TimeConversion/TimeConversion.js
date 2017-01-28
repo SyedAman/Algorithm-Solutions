0dad054b8f819655c81690df4695278202f14a3b
Syed Aman <sna274@nyu.edu>
2017-01-27 22:27:44 -0500

    fix edge cases

--- HackerRank/TimeConversion/TimeConversion.js
+++ HackerRank/TimeConversion/TimeConversion.js
@@ -31,14 +31,34 @@ function main() {
     var minute = timeArray.splice(0, 1);
     var second = timeArray.splice(0, 1);
 
-    // add 12 hours to hour if PM
-    if (second[0].includes('PM')) {
+    // add 12 hours to hour if 1:00:00PM - 11:59:59PM
+    if (second[0].includes('PM') && hour != 12) {
         hour = parseInt(hour);
         hour += 12;
     }
 
+    // add 12 hours to hour if 1:00:00PM - 11:59:59PM
+    if (second[0].includes('PM') && hour != 12) {
+        hour = parseInt(hour);
+        hour += 12;
+    }
+
+    // subtract 12 hours to hour if 12:00:00AM - 12:59:59AM
+    if (second[0].includes('AM') && hour == 12) {
+        hour = parseInt(hour);
+        hour -= 12;
+    }
+
+    if (hour == 24) {
+        hour = 0;
+    }
+
     // remove the 'AM' or 'PM' from second
     second[0] = second[0].substr(0, second[0].length-2);
+
+    if(hour >= 0 && hour < 12) {
+        hour = '0' + hour;
+    }
 
     console.log(hour + ':' + minute + ':' + second[0]);
 }
